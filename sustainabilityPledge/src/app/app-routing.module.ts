8c57acee6e586e1a8f36cd471c4381fb3456dc24
Chris Martinez <cmartinez5@ggc.edu>
2020-03-29 10:40:28 -0400

    I added the login code and modified the button in the submit page

--- sustainabilityPledge/src/app/app-routing.module.ts
+++ sustainabilityPledge/src/app/app-routing.module.ts
@@ -1,44 +1,29 @@
-import { TransportationComponent } from './transportation/transportation.component';
-<<<<<<< HEAD
-import { RecyclingComponent } from './recycling/recycling.component';
-import { FoodComponent } from './food/food.component';
-import { ElectricityComponent } from './electricity/electricity.component';
-import { WaterComponent } from './water/water.component';
-import { SignupComponent } from './signup/signup.component';
-=======
-import { ElectricityComponent } from './electricity/electricity.component';
-import { RecyclingComponent } from './recycling/recycling.component';
-import { FoodComponent } from './food/food.component';
-import { WaterComponent } from './water/water.component';
-import { SignupComponent } from './signup/signup.component';
 import { LoginComponent } from './login/login.component';
->>>>>>> part2
+import { TransportationComponent } from './transportation/transportation.component';
+
+import { RecyclingComponent } from './recycling/recycling.component';
+import { FoodComponent } from './food/food.component';
+import { ElectricityComponent } from './electricity/electricity.component';
+import { WaterComponent } from './water/water.component';
+import { SignupComponent } from './signup/signup.component';
+
 import { HomeComponent } from './home/home.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 
 const routes: Routes = [
-<<<<<<< HEAD
+
   {path: '', redirectTo: '/home', pathMatch: 'full'},
   {path: 'home', component: HomeComponent},
   {path: 'signup', component: SignupComponent},
+  {path: 'login', component: LoginComponent},
   {path: 'water', component: WaterComponent},
   {path: 'electricity', component: ElectricityComponent},
   {path: 'food', component: FoodComponent},
   {path: 'recycling', component: RecyclingComponent},
   {path: 'transportation', component: TransportationComponent}
-=======
-  { path: '', redirectTo: '/home', pathMatch: 'full'},
-  { path: 'home', component: HomeComponent},
-  { path: 'signup', component: SignupComponent},
-  { path: 'login', component: LoginComponent},
-  { path: 'water', component: WaterComponent},
-  { path: 'food', component: FoodComponent},
-  { path: 'recycling', component: RecyclingComponent},
-  { path: 'electricity', component: ElectricityComponent},
-  { path: 'transportation', component: TransportationComponent},
->>>>>>> part2
+
 ];
 
 @NgModule({
--- sustainabilityPledge/src/app/app.module.ts
+++ sustainabilityPledge/src/app/app.module.ts
@@ -5,42 +5,27 @@ import { FormsModule} from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
-<<<<<<< HEAD
-=======
 import { LoginComponent } from './login/login.component';
->>>>>>> part2
 import { SignupComponent } from './signup/signup.component';
 import { WaterComponent } from './water/water.component';
 import { ElectricityComponent } from './electricity/electricity.component';
 import { FoodComponent } from './food/food.component';
-<<<<<<< HEAD
-import { TransportationComponent } from './transportation/transportation.component';
-import { RecyclingComponent } from './recycling/recycling.component';
-=======
 import { RecyclingComponent } from './recycling/recycling.component';
 import { TransportationComponent } from './transportation/transportation.component';
 
->>>>>>> part2
+
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-<<<<<<< HEAD
-=======
     LoginComponent,
->>>>>>> part2
     SignupComponent,
     WaterComponent,
     ElectricityComponent,
     FoodComponent,
-<<<<<<< HEAD
-    TransportationComponent,
-    RecyclingComponent
-=======
     RecyclingComponent,
     TransportationComponent
->>>>>>> part2
   ],
   imports: [
     FormsModule,
