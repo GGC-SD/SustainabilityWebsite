5721c2f0d39f5f31ba8ce686daf0e096c1fd25a7
Angel Mercado <amercadorodrigue@ggc.edu>
2020-11-11 18:45:03 -0500

    Preparing to host

--- sustainabilityPledge/src/environments/environment.prod.ts
+++ sustainabilityPledge/src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase : {
+    apiKey: "AIzaSyAzAipR-eknKeFFYKI0iJgRNdya0BV1q5E",
+    authDomain: "sustanaibility-pledge.firebaseapp.com",
+    databaseURL: "https://sustanaibility-pledge.firebaseio.com",
+    projectId: "sustanaibility-pledge",
+    storageBucket: "sustanaibility-pledge.appspot.com",
+    messagingSenderId: "25534878445",
+    appId: "1:25534878445:web:5ce3595076907e8c55b2e4",
+    measurementId: "G-K9VME3KXL8"
+  }
 };
--- sustainabilityPledge/src/environments/environment.ts
+++ sustainabilityPledge/src/environments/environment.ts
@@ -3,7 +3,7 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false,
+  production: true,
   firebase : {
     apiKey: "AIzaSyAzAipR-eknKeFFYKI0iJgRNdya0BV1q5E",
     authDomain: "sustanaibility-pledge.firebaseapp.com",
